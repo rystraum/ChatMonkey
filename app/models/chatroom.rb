557615bea16aeb29892e8ab58666b6ff274f196c
Rystraum Gamonez <rystraum@gmail.com>
2011-11-07 16:11:07 +0800

    added chatroom and user has many msgs

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -1,3 +1,4 @@
 class Chatroom < ActiveRecord::Base
   has_and_belongs_to_many :users
+  has_many :msgs
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,5 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me
 
   has_and_belongs_to_many :chatrooms
+  has_many :msgs
 end
