553add781aa57cb51a8f78545e9c5f189737d06b
Rystraum Gamonez <rystraum@gmail.com>
2011-11-07 21:19:59 +0800

    added create msg method

--- app/controllers/msgs_controller.rb
+++ app/controllers/msgs_controller.rb
@@ -1,5 +1,13 @@
 class MsgsController < ApplicationController
   def create
+    @chatroom = Chatroom.find(params[:chatroom_id])
+    @msg = @chatroom.msgs.create!(params[:msg])
+    current_user.msgs << @msg
+
+    respond_to do |format|
+      format.html { redirect_to @chatroom }
+      format.js { render :nothing => true }
+    end
   end
 
 end
