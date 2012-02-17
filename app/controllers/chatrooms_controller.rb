06181ecedf36f7198f2cd15d3eb6dc7ec2762556
Rystraum Gamonez <rystraum@gmail.com>
2012-02-17 19:27:30 +0800

    added licensing notifications

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,3 +1,8 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+# (c) Rystraum Gamonez
+
 class ChatroomsController < ApplicationController
   before_filter :authenticate_user!
   before_filter :user_is_owner, :only => [:edit, :update, :destroy]
--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -1,3 +1,8 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+# (c) Rystraum Gamonez
+
 class Chatroom < ActiveRecord::Base
   has_and_belongs_to_many :users
   has_many :msgs
