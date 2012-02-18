fc168c57961072784128601d486f31ce996953f6
Rystraum Gamonez <rystraum@gmail.com>
2012-02-19 02:40:59 +0800

    more spec files

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
 
 describe User do
-  context "valid attributes" do
-    before :each do
+  before :each do
       @user = Factory.attributes_for :user
-    end
+  end
 
+  context "valid attributes" do
     it "should be successful" do
       User.create! @user
     end
@@ -13,7 +13,6 @@ describe User do
 
   context "if without a display_name" do
     before :each do
-      @user = Factory.attributes_for :user
       @user.delete :display_name
     end
 
@@ -27,5 +26,34 @@ describe User do
     end
   end
 
+  context "invalid emails" do
+    it "should require an email" do
+      @user.delete :email
+      no_email = User.new @user
+      no_email.should_not be_valid
+    end
 
+    it "should not accept emails without domains" do
+      bad = User.new(@user.merge email: "wrong@")
+      bad.should_not be_valid
+    end
+
+    it "should not accept emails without username" do
+      bad = User.new(@user.merge email: "@domain.com")
+      bad.should_not be_valid
+    end
+  end
+
+  context "passwords" do
+    it "should require a password" do
+      @user.delete :password
+      bad = User.new @user
+      bad.should_not be_valid
+    end
+
+    it "should require that the password is the same as the confirmation" do
+      bad = User.new(@user.merge password_confirmation: @user[:password] + "1" )
+      bad.should_not be_valid
+    end
+  end
 end
