6e90bf80868e96d2ad3f21521817c4c91d3bbb9a
Rystraum Gamonez <rystraum@gmail.com>
2012-02-19 15:14:42 +0800

    added voting method

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -2,7 +2,15 @@ class SuggestionsController < InheritedResources::Base
   respond_to :html, :js
 
   def create
-    create! { suggestions_url }
+    create! { suggestions_path }
   end
 
+  def vote
+    resource.votes += 1
+    if resource.save
+      redirect_to suggestions_path, flash: { success: "Thanks for voting!" }
+    else
+      redirect_to suggestions_path, flash: { error: "OHNO!" }
+    end
+  end
 end
