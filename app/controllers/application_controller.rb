5c850c24f5a9c3f1a02462228b7416ec28420e14
madumlao <madumlao@gmail.com>
2014-12-02 00:19:30 +0800

    Allow authentication using HTTP basic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :authenticate_user_from_http_basic!
+  
+  def authenticate_user_from_http_basic!
+    if (request.authorization)
+      authorization = ActionController::HttpAuthentication::Basic.user_name_and_password(request)
+      email = authorization[0]
+      password = authorization[1]
+    end
+    
+    user = User.find_for_authentication email: email
+    if user && user.valid_password?(password)
+      sign_in user, store: false
+    end
+  end
 end
